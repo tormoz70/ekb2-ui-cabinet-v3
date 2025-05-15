eeb3a267341a7f66421310d2777a164fa37af9ab
Ayrat <gpsdom@gmail.com>
2025-05-15 22:50:03 +0300

    поправил пагинацию в гриде не правильно работает навигация в гриде с помощью клавиатуры

--- src/components/dgrid/DGrid.tsx
+++ src/components/dgrid/DGrid.tsx
@@ -1,15 +1,17 @@
 import {
-    IconButton,
-    styled, TablePagination
+    Box,
+    IconButton, MenuItem, Pagination, Select,
+    styled, TablePagination, TablePaginationProps
 } from "@mui/material";
 import colorConfigs from "../../configs/colorConfigs";
 import {localConfigs} from "../../configs/localConfigs";
 import {
     DataGrid, DataGridProps,
     gridPageCountSelector,
-    gridPageSelector,
+    gridPageSelector, gridPaginationModelSelector,
     useGridApiContext,
     useGridSelector,
+    GridPagination
 } from '@mui/x-data-grid';
 import {
     FirstPage as FirstPageIcon,
@@ -17,6 +19,8 @@ import {
     KeyboardArrowLeft,
     KeyboardArrowRight,
 } from '@mui/icons-material';
+import MuiPagination from '@mui/material/Pagination';
+
 
 interface CustomDataGridProps extends DataGridProps {
     heightDeltaMinus: number;
@@ -42,48 +46,33 @@ const StyledDataGrid = styled(DataGrid)<CustomDataGridProps>(({ theme, heightDel
 
 }));
 
-function CustomPaginationActions() {
+function PPagination({
+                        page,
+                        onPageChange,
+                        className,
+                    }: Pick<TablePaginationProps, 'page' | 'onPageChange' | 'className'>) {
     const apiRef = useGridApiContext();
-    const page = useGridSelector(apiRef, gridPageSelector);
     const pageCount = useGridSelector(apiRef, gridPageCountSelector);
 
     return (
-        <div style={{ display: 'flex' }}>
-            <IconButton
-                onClick={() => apiRef.current.setPage(0)}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                <FirstPageIcon />
-            </IconButton>
-            <IconButton
-                onClick={() => apiRef.current.setPage(page - 1)}
-                disabled={page === 0}
-                aria-label="previous page"
-            >
-                <KeyboardArrowLeft />
-            </IconButton>
-            <IconButton
-                onClick={() => apiRef.current.setPage(page + 1)}
-                disabled={page >= pageCount - 1}
-                aria-label="next page"
-            >
-                <KeyboardArrowRight />
-            </IconButton>
-            <IconButton
-                onClick={() => apiRef.current.setPage(pageCount - 1)}
-                disabled={page >= pageCount - 1}
-                aria-label="last page"
-            >
-                <LastPageIcon />
-            </IconButton>
-        </div>
+        <MuiPagination
+            color="primary"
+            className={className}
+            count={pageCount}
+            page={page + 1}
+            onChange={(event, newPage) => {
+                onPageChange(event as any, newPage - 1);
+            }}
+            showFirstButton
+            showLastButton
+        />
     );
 }
 
-function CustomPagination(props) {
-    return <TablePagination ActionsComponent={CustomPaginationActions} {...props} />;
+function CustomPagination(props: any) {
+    return <GridPagination ActionsComponent={PPagination} {...props} />;
 }
+
 
 function DGrid(props: CustomDataGridProps) {
     return (
